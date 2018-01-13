631a2a05699ef3887381de3ed87f73a6c20e0dbc
Humberto Padron <xnsmiami@gmail.com>
2018-01-13 21:54:52 +0000

    Add email with sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'dispatch2018-app.herokuapp.com/'}
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -38,4 +40,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.delivery_method = :test
+
+ config.action_mailer.default_url_options = { :host => 'https://dispatch2018-app.herokuapp.com/'}
 end
